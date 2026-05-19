513cf6b23a0b6965e5a38d0b6c0edd3eaf83128b
Ayyaz <ayyazali.sajjadali@gmail.com>
2026-05-19 21:57:26 +0500

    Load Firebase config from external script

---
+++ app-config.js
@@ -0,0 +1,11 @@
+window.APPVION_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC44enadsgl9w1a0mRE51zaLY-1uc5AWwI",
+    authDomain: "appvion-studio.firebaseapp.com",
+    projectId: "appvion-studio",
+    storageBucket: "appvion-studio.firebasestorage.app",
+    messagingSenderId: "4195907711",
+    appId: "1:4195907711:web:3cdf11646b9b7bc876e74a",
+    measurementId: "G-DEV3NRR902"
+};
+
+window.APPVION_FIREBASE_CONTENT_PATH = ["siteContent", "home"];
